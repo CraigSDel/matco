2b4b60d1349c3f387ded6c4d23522c10811f1d1a
Craig Stroberg <strobergcd@gmail.com>
2019-06-09 16:46:47 +0200

    database changes

--- matco_p.dpr
+++ matco_p.dpr
@@ -3,11 +3,13 @@ program matco_p;
 uses
   Vcl.Forms,
   main_u in 'main_u.pas' {Matco},
-  role_u in 'role\Role_u.pas',
+  Role_u in 'role\Role_u.pas',
   dmMatco_u in 'dmMatco_u.pas' {DMMatco: TDataModule},
   RoleService_u in 'role\RoleService_u.pas',
   RoleResource_u in 'role\RoleResource_u.pas',
-  Ticket_u in 'ticket\Ticket_u.pas';
+  Ticket_u in 'ticket\Ticket_u.pas',
+  TicketService_u in 'ticket\TicketService_u.pas',
+  TicketResource_u in 'ticket\TicketResource_u.pas';
 
 {$R *.res}
 
