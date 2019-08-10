b8d09511c679c6744ef79741816c8c1ca9fd0da7
Craig Stroberg <strobergcd@gmail.com>
2019-08-10 12:25:22 +0200

    :)

--- matco_p.dpr
+++ matco_p.dpr
@@ -4,16 +4,16 @@ uses
   Vcl.Forms,
   dmMatco_u in 'dmMatco_u.pas' {DMMatco: TDataModule},
   frmMatco_u in 'frmMatco_u.pas' {frmMatco},
-  frmUser_u in 'user\frmUser_u.pas' {frmUser},
   User_u in 'user\User_u.pas',
   client_u in 'client\client_u.pas',
+  ProjectTicket_u in 'project_ticket\ProjectTicket_u.pas',
+  project_u in 'project\project_u.pas',
+  Ticket_u in 'ticket\Ticket_u.pas',
   frmClient_u in 'client\frmClient_u.pas' {frmClient},
   frmProjectTicket_u in 'project_ticket\frmProjectTicket_u.pas' {frmProjectTicket},
-  ProjectTicket_u in 'project_ticket\ProjectTicket_u.pas',
   frmProject_u in 'project\frmProject_u.pas' {frmProject},
-  project_u in 'project\project_u.pas',
   frmTicket_u in 'ticket\frmTicket_u.pas' {frmTicket},
-  Ticket_u in 'ticket\Ticket_u.pas';
+  frmUser_u in 'user\frmUser_u.pas' {frmUser};
 
 {$R *.res}
 
@@ -22,13 +22,8 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrmMatco, frmMatco);
   Application.CreateForm(TDMMatco, DMMatco);
-  Application.CreateForm(TfrmUser, frmUser);
   Application.CreateForm(TfrmClient, frmClient);
   Application.CreateForm(TfrmProject, frmProject);
-  Application.CreateForm(TfrmProjectTicket, frmProjectTicket);
-  Application.CreateForm(TfrmProjectTicket, frmProjectTicket);
-  Application.CreateForm(TfrmProject, frmProject);
-  Application.CreateForm(TfrmTicket, frmTicket);
   Application.Run;
 end.
 
